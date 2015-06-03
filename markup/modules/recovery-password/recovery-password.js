1c37a120ad52b6fee3d58bf44bfda89f36bf1e97
Mik <mik639inc@gmail.com>
2015-06-03 10:45:23 +0400

    Правка в наименовании страницы

--- markup/modules/recovery-password/recovery-password.js
+++ markup/modules/recovery-password/recovery-password.js
@@ -1,7 +1,7 @@
-$('#recovery_password-form').submit(function () {
+$('#recovery-password-form').submit(function () {
     sendForm($(this).attr('action'), $(this).attr('method'), $(this).attr('id'),
         function (response) {
-            $('#recovery_password-form').html('<p class="recovery_password-block__success-msg">Пароль выслан на указанный телефон и Email</p>');
+            $('#recovery-password-form').html('<p class="recovery-password-block__success-msg">Пароль выслан на указанный телефон и Email</p>');
         });
     return false;
 });
