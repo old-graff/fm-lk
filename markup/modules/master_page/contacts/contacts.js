0a2049a8a387ed5f48ea88fbfb3f8c6d23906ff3
dumperize <ole.elena@gmail.com>
2015-07-30 13:00:14 +0400

    Обратный звонок на странице мастера

--- markup/modules/master_page/contacts/contacts.js
+++ markup/modules/master_page/contacts/contacts.js
@@ -2,3 +2,11 @@ $('.js-contacts-show').click(function () {
     $(this).hide();
     $(this).parent().find('.js-contacts-content').show();
 });
+$('.js-callback-popup').click(function () {
+    var form = $('.callback_popup');
+    if (form.length > 0) {
+        $.fancybox.open(form, {
+            padding: 0
+        });
+    }
+});
